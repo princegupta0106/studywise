4635977c5ab78b65031eb2122e0e677f6019a5d1
Prince Gupta <princeguptapg0106@gmail.com>
2025-11-17 22:10:04 +0530

    changed permission for google crawlers

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext'
